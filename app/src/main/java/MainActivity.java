21152fcf3a6a37cb012a1aa4f0aa12fc93198419
Sudarshan Rajitha <info.srsolutions.dev@gmail.com>
2026-01-28 20:49:54 +0530

    Added Android application code with MainActivity

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,10 @@
+package com.tourist;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Tourist-Guide-Android
+console.log('Application starting...');
+
+function main() {
+  console.log('Main function executed');
+}
+
+main();
